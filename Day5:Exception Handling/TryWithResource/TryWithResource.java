50619cf82c21f9bc493357baabfa87ef898f20e9
Mamatha S <129306547+Harshitha9606@users.noreply.github.com>
2025-12-15 15:10:19 +0530

    Add files via upload

---
+++ Day5:Exception Handling/TryWithResource/TryWithResource.java
@@ -0,0 +1,23 @@
+package Day5Excercise;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResource {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Sample.txt"))) 
+        {
+            String line;
+            while ((line = br.readLine()) != null) 
+            {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) 
+        {
+            System.out.println("File error: " + e.getMessage());
+        }
+    }
+}
