3e1842c324f6e3dca4a83423627f699437396e9c
Mamatha S <129306547+Harshitha9606@users.noreply.github.com>
2025-12-15 16:03:56 +0530

    Add files via upload

---
+++ Day6:Threads/PrintNumbers/PrintNumber.java
@@ -0,0 +1,14 @@
+package Day6Excercise;
+
+public class PrintNumber extends Thread {
+	public void run()
+	{
+		for(int i=1;i<=10;i++)
+		{
+		System.out.println(i);
+		}
+	}
+		
+}
+
+
---
+++ Day6:Threads/PrintNumbers/PrintNumberMain.java
@@ -0,0 +1,11 @@
+package Day6Excercise;
+
+public class PrintNumberMain {
+
+	public static void main(String[] args) {
+		
+		PrintNumber number=new PrintNumber();
+		number.start();
+	}
+
+}
